4860aacb5469043f95a76530718c4a290cf1a319
KONE <largaton95@gmail.com>
2023-10-01 14:50:26 +0200

    Attributq

---
+++ src/pk/CCJava/Commune.java
@@ -0,0 +1,17 @@
+package pk.CCJava;
+
+public class Commune {
+
+    private String name;
+    private int department;
+    private int postalCode;
+    private int population;
+    private int surface;
+    
+
+
+
+
+
+    
+}
