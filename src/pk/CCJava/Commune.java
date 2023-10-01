755867b0d105fbe5373bfb9dbd4216754c64948c
KONE <largaton95@gmail.com>
2023-10-01 14:52:56 +0200

    Constructeur vide et trivial

--- src/pk/CCJava/Commune.java
+++ src/pk/CCJava/Commune.java
@@ -7,11 +7,19 @@ public class Commune {
     private int postalCode;
     private int population;
     private int surface;
-    
 
+    // Constructeur vide
+    Commune() {
+    }
 
+    // Constructeur trivial
+    Commune(String name, int department, int postalCode, int population, int surface) {
+        this.name = name;
+        this.department = department;
+        this.postalCode = postalCode;
+        this.population = population;
+        this.surface = surface;
 
+    }
 
-
-    
 }
